2458cf4668bae945f26aed365bcecd1821c10fdb
Type 999B <143522379+alex226336@users.noreply.github.com>
2024-10-06 13:38:06 +1100

    c

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed;
+
+    public float unitMovement;
+    public bool isMoving;
+
+    private Vector2 input;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +19,36 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        if(!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+            if (input != Vector2.zero)
+            {
+                var targetPos = transform.position;
+                targetPos.x += input.x*0.1f;
+                targetPos.y += input.y*0.1f;
+                Debug.Log(targetPos.y);
+
+                StartCoroutine(Move(targetPos));
+            }
+        }
         
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        while ((targetPos-transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+
+        isMoving = false;
+
     }
 }
